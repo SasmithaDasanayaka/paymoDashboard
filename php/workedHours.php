7f2bdc36a0929915af40345882d96c2ddef1b9dd
Sasmitha Dasanayaka <sasmithadasanayaka96@gmail.com>
2020-11-18 00:04:06 +0530

    worked hours with month filter

--- php/workedHours.php
+++ php/workedHours.php
@@ -1,5 +1,5 @@
 <?php
-$projectsUrl = "https://app.paymoapp.com/api/projects";
+$projectsUrl = "https://app.paymoapp.com/api/clients";
 $email = "sasmithadasanayaka96@gmail.com";
 $password = "HjA!!7P2Mtxhu5b";
 
@@ -17,13 +17,17 @@ curl_close($ch);
 
 $projectsArr = array();
 $totalTime = 0;
-// echo $result;
-foreach (json_decode($result, true)['projects'] as $project) {
-    array_push($projectsArr, $project['name']);
-    $projectId =  $project['id'];
 
-    $timeUrl = "https://app.paymoapp.com/api/entries?where=project_id=$projectId";
+foreach (json_decode($result, true)['clients'] as $client) {
+    $clientId =  $client['id'];
 
+    $currentDate = gmdate('y-m-d');
+    $previousDate = date_create(gmdate('y-m-d'));
+    date_sub($previousDate, date_interval_create_from_date_string("30 days"));
+    $newPreviousDate = date_format($previousDate, "Y-m-d");
+
+    $timeUrl = "https://app.paymoapp.com/api/entries?where=client_id=$clientId and time_interval in ('$newPreviousDate','$currentDate')";
+    
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $timeUrl);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
@@ -33,12 +37,14 @@ foreach (json_decode($result, true)['projects'] as $project) {
     $result = curl_exec($ch);
     if ($result === false) {
         echo "Curl error: " . curl_error($ch) . "\n";
+        exit();
     }
     curl_close($ch);
-    print_r(json_decode($result, true));
+
     foreach (json_decode($result, true)['entries'] as $entry) {
         $totalTime += $entry['duration'];
     }
 }
+$totalHoursWorked = $totalTime / 3600;
 
-print_r($totalTime / 3600);
+echo $totalHoursWorked ;
