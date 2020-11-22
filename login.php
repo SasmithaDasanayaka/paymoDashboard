d1327732122cf0fd5504c0bb4936f4a97e866af8
Sasmitha Dasanayaka <sasmithadasanayaka96@gmail.com>
2020-11-22 12:40:43 +0530

    removed unwanted comments

--- login.php
+++ login.php
@@ -22,52 +22,6 @@ if (isset($_POST['Submit'])) {
 }
 ?>
 
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body {
-            font: 14px sans-serif;
-        }
-
-        .wrapper {
-            width: 350px;
-            padding: 20px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="" method="post" name="Login_Form">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="Username" class="form-control">
-            </div>
-            <div class="form-group ">
-                <label>Password</label>
-                <input type="password" name="Password" class="form-control">
-            </div>
-            <div class="form-group">
-                <input name="Submit" type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <?php if (isset($msg)) { ?>
-
-                <?php echo $msg; ?>
-
-            <?php } ?>
-        </form>
-    </div>
-</body>
-
-</html> -->
-
 <!DOCTYPE html>
 <html>
   <head>
